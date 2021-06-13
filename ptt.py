34e7e9cf1edf42f67e0f34571fc398c9f66d9a86
nevikw39 <nevik.w39@gmail.com>
2021-06-13 22:33:04 +0800

    Flow

--- ptt.py
+++ ptt.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-CMD, LST, ADD, ADD_ARGS, REMOVE, REMOVE_ARGS, QUERY, QUERY_ARGS = range(8)
+CMD, ADD, REMOVE, QUERY = range(4)
 
 
 def ptt(update: Update, context: CallbackContext) -> int:
@@ -33,51 +33,42 @@ def ptt(update: Update, context: CallbackContext) -> int:
 
 
 def cmd(update: Update, context: CallbackContext) -> int:
-    return {"add": ADD, "lst": LST, "remove": REMOVE, "query": QUERY}[update.message.text]
+    if update.message.text == "lst":
+        try:
+            db.cur.execute("SELECT id FROM ptt")
+            update.message.reply_text(db.cur.fetchall())
+        except Exception as e:
+            update.message.reply_text(repr(e))
 
-
-def lst(update: Update, context: CallbackContext) -> int:
-    try:
-        db.cur.execute("SELECT id FROM ptt")
-        update.message.reply_text(db.cur.fetchall())
-    except Exception as e:
-        update.message.reply_text(e)
-
-    return ConversationHandler.END
+        return ConversationHandler.END
+    elif update.message.text == "add":
+        update.message.reply_text('Please input ids.')
+        return ADD
+    elif update.message.text == "remove":
+        update.message.reply_text('Please input ids.')
+        return REMOVE
 
 
 def add(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text('Please input ids.')
-
-    return ADD_ARGS
-
-
-def add_args(update: Update, context: CallbackContext) -> int:
     try:
         for id in update.message.text.split('\n'):
             db.cur.execute(f"INSERT INTO ptt VALUES ({id})")
         db.conn.commit()
         update.message.reply_text("Succesfully added.")
     except Exception as e:
-        update.message.reply_text(e)
+        update.message.reply_text(repr(e))
 
     return ConversationHandler.END
 
 
 def remove(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text('Please input ids.')
-
-    return ADD_ARGS
-
-
-def remove_args(update: Update, context: CallbackContext) -> int:
     try:
         for id in update.message.text.split('\n'):
             db.cur.execute(f"DELETE FROM ptt WHERE id = {id}")
         db.conn.commit()
         update.message.reply_text("Succesfully added.")
     except Exception as e:
-        update.message.reply_text(e)
+        update.message.reply_text(repr(e))
 
     return ConversationHandler.END
 
@@ -93,13 +84,9 @@ conv_handler = ConversationHandler(
     entry_points=[CommandHandler('ptt', ptt)],
     states={
         CMD: [MessageHandler(Filters.regex('^(add|lst|remove|query)$'), cmd)],
-        LST: [MessageHandler(Filters.text, lst)],
         ADD: [MessageHandler(Filters.text, add)],
-        ADD_ARGS: [MessageHandler(Filters.text, add_args)],
         REMOVE: [MessageHandler(Filters.text, remove)],
-        REMOVE_ARGS: [MessageHandler(Filters.text, remove_args)],
         # QUERY: [MessageHandler(Filters.text, query)],
-        # QUERY_ARGS: [MessageHandler(Filters.text, query_args)],
     },
     fallbacks=[CommandHandler('cancel', cancel)],
 )
