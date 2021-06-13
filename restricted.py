c7efe706adc08385fe37edd18bdea3086ad3a2f3
nevikw39 <nevik.w39@gmail.com>
2021-06-13 23:57:40 +0800

    fix restricted

--- restricted.py
+++ restricted.py
@@ -1,10 +1,13 @@
 from functools import wraps
 
-LIST_OF_ADMINS = (692286133)
+from telegram import Update
+from telegram.ext import CallbackContext
+
+LIST_OF_ADMINS = {692286133}
 
 def restricted(func):
     @wraps(func)
-    def wrapped(update, context, *args, **kwargs):
+    def wrapped(update: Update, context: CallbackContext, *args, **kwargs):
         user_id = update.effective_user.id
         if user_id not in LIST_OF_ADMINS:
             print("Unauthorized access denied for {}.".format(user_id))
