0bcc17a6feb21fc7bd2ece310cb73df32616e064
nevikw39 <nevik.w39@gmail.com>
2021-06-13 17:37:08 +0800

    Bot Commands

--- main.go
+++ main.go
@@ -27,6 +27,30 @@ func main() {
 	go http.ListenAndServe("0.0.0.0:"+os.Getenv("PORT"), nil)
 
 	for update := range updates {
-		log.Printf("%+v\n", update)
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			switch update.Message.Command() {
+			case "help":
+				msg.Text = "type /sayhi or /status."
+			case "sayhi":
+				msg.Text = "Hi :)"
+			case "status":
+				msg.Text = "I'm ok."
+			case "withArgument":
+				msg.Text = "You supplied the following argument: " + update.Message.CommandArguments()
+			case "html":
+				msg.ParseMode = "html"
+				msg.Text = "This will be interpreted as HTML, click <a href=\"https://www.example.com\">here</a>"
+			default:
+				msg.Text = "I don't know that command"
+			}
+			bot.Send(msg)
+		}
 	}
 }
